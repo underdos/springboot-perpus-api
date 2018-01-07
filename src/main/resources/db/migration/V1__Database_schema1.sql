3bcac208fe807a4c1f3b8d9e015723a36519eaba
Kusnadi <kus.underdos@gmail.com>
2018-01-07 17:21:11 +0700

    update & rename table on db migration

--- src/main/resources/db/migration/V1__Database_schema1.sql
+++ src/main/resources/db/migration/V1__Database_schema1.sql
@@ -1,4 +1,4 @@
-create table user (
+create table t_mtr_user (
     id SERIAL NOT NULL,
     name ARCHAR(100) NOT NULL,
     email VARCHAR(100) NOT NULL,
--- src/main/resources/db/migration/V2__Init_data1.sql
+++ src/main/resources/db/migration/V2__Init_data1.sql
@@ -1,3 +1,3 @@
-insert into user (name, email) VALUES ("Kusnadi", "kus.underdos@gmail.com");
-insert into user (name, email) VALUES ("Kunay", "kunay@gmail.com")
-insert into user (name, email) VALUES ("Encus", "encus@gmail.com");
+insert into t_mtr_user (name, email) VALUES ("Kusnadi", "kus.underdos@gmail.com");
+insert into t_mtr_user (name, email) VALUES ("Kunay", "kunay@gmail.com")
+insert into t_mtr_user (name, email) VALUES ("Encus", "encus@gmail.com");
