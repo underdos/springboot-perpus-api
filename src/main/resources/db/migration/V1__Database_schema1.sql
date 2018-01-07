fa779931e46d20409badcb6c5ddbba2527e62723
Kusnadi <kus.underdos@gmail.com>
2018-01-07 17:16:55 +0700

    Update db migration

--- src/main/resources/db/migration/V1__Database_schema1.sql
+++ src/main/resources/db/migration/V1__Database_schema1.sql
@@ -1,6 +1,6 @@
-CREATE TABLE user (
+create table user (
     id SERIAL NOT NULL,
-    name VARCHAR(100) NOT NULL,
+    name ARCHAR(100) NOT NULL,
     email VARCHAR(100) NOT NULL,
     PRIMARY KEY (id)
 );
