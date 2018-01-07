98159108653669202f11dccbb4f8086baf4a4456
Kusnadi <kus.underdos@gmail.com>
2018-01-07 16:32:42 +0700

    First commit API for Perpustakaan app

---
+++ src/main/java/net/kusnadi/perpus/perpus/PerpusApplication.java
@@ -0,0 +1,12 @@
+package net.kusnadi.perpus.perpus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerpusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerpusApplication.class, args);
+	}
+}
---
+++ src/test/java/net/kusnadi/perpus/perpus/PerpusApplicationTests.java
@@ -0,0 +1,16 @@
+package net.kusnadi.perpus.perpus;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PerpusApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
